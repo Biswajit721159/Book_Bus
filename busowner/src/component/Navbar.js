26b2e97faff64626c36661554346e15cd7b7d236
Biswajit721159 <bg5050525@gmail.com>
2023-10-03 09:56:02 +0530

    adding more

--- busowner/src/component/Navbar.js
+++ busowner/src/component/Navbar.js
@@ -47,11 +47,11 @@ const Navbar =()=>{
                                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Welcome {userinfo.user.name}
                                     </a>
-                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        {/* <li><a className="dropdown-item" href="#">Action</a></li>
+                                    {/* <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a className="dropdown-item" href="#">Action</a></li>
                                         <li><a className="dropdown-item" href="#">Another action</a></li>
-                                        <li><a className="dropdown-item" href="#">Something else here</a></li> */}
-                                    </ul>
+                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                    </ul> */}
                                 </li>
                             </>
                        }
