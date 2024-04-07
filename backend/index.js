8997a59284ee5f74bb3334015ebb3f6f3dd3aac0
Biswajit721159 <bg5050525@gmail.com>
2024-04-07 20:21:09 +0530

    adding more

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,26 @@ app.use(express.static("public"))
 
 connectDB()
 
-let mainRouter = require('./src/Router/mainRouter')
+let verifytoken=require('./src/utils/ApiResponce')
+let user = require('./src/Router/userRouter')
+let MasterList = require('./src/Router/MasterListRouter')
+let bus = require('./src/Router/Bus_detailRouter')
+let booking = require('./src/Router/BookingRouter')
+let busowner = require('./src/Router/BusOwnerUserRouter')
+let Adminpanel = require('./src/Router/Adminpanel_userRouter')
+let sendMessage = require('./src/Router/sendMessageRoute')
+
+app.use('/user', user)
+app.use('/MasterList', MasterList)
+app.use('/bus', bus)
+app.use('/Booking', booking)
+app.use('/busowner', busowner)
+app.use('/Adminpanel', Adminpanel)
+app.use('/sendMessage', sendMessage)
 
 app.get('/', async (req, res) => {
   res.send("Server is Running clearly")
 })
-
-app.use('/api', mainRouter)
 
 
 app.listen(5000);
