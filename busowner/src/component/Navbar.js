3973029d52fc6224b1fada50ead9ffd6782eb4a0
Biswajit721159 <bg5050525@gmail.com>
2024-09-21 21:41:32 +0530

    changes the navbar color issue

--- busowner/src/component/Navbar.js
+++ busowner/src/component/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="bg-blue-500 shadow text-white">
+        <nav className="bg-blue-500 shadow text-white bg-gradient-to-r from-sky-500 to-indigo-500">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16 items-center">
                     <div className="flex items-center">
