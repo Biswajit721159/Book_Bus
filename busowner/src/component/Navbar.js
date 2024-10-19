93baf13332999497cb1216c0f64ea7d2373fc084
Biswajit721159 <bg5050525@gmail.com>
2024-10-19 23:38:19 +0530

    add the side bar

--- busowner/src/component/Navbar.js
+++ busowner/src/component/Navbar.js
@@ -1,15 +1,143 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import { useDispatch, useSelector } from "react-redux";
 import { usermethod } from '../redux/userSlice'
 import { toast } from 'react-toastify'
+import { IconButton } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Person2Icon from '@mui/icons-material/Person2';
+import DirectionsBusFilledIcon from '@mui/icons-material/DirectionsBusFilled';
+import BookOnlineIcon from '@mui/icons-material/BookOnline';
+import LogoutIcon from '@mui/icons-material/Logout';
+import RecentActorsIcon from '@mui/icons-material/RecentActors';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home';
 
 const Navbar = () => {
     const userinfo = useSelector((state) => state.userAuth.user);
     const otherUserinfo = useSelector((state) => state.userAuth.otherUserinfo);
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const history = useNavigate();
+
+    const list = (anchor) => (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={() => setDrawerOpen(false)}
+            onKeyDown={() => setDrawerOpen(false)}
+        >
+            <List>
+
+                <ListItem>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <AccountCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary={userinfo?.user.fullName}
+                        />
+                    </ListItemButton>
+                </ListItem>
+                <Divider />
+                <ListItem onClick={() => history('/')}>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary={'Home'} />
+                    </ListItemButton>
+                </ListItem>
+
+                {otherUserinfo.role === '100' ?
+                    <>
+                        <ListItem onClick={() => history('/BusAdder')}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Person2Icon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={'Add Bus'} />
+                            </ListItemButton>
+                        </ListItem>
+
+                        <ListItem onClick={() => history('/ViewSeat')}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <BookOnlineIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={'View Seat'} />
+                            </ListItemButton>
+                        </ListItem>
+                    </>
+                    :
+                    <>
+                        <ListItem onClick={() => history('/ManageUser')}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Person2Icon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={'Manage User'} />
+                            </ListItemButton>
+                        </ListItem>
+
+                        <ListItem onClick={() => history('/Booking')}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <BookOnlineIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={'Manage Booking'} />
+                            </ListItemButton>
+                        </ListItem>
+
+                        <ListItem onClick={() => history('/ManageBus')}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <DirectionsBusFilledIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={'Manage Bus'} />
+                            </ListItemButton>
+                        </ListItem>
+
+                        <ListItem onClick={() => history('/ManageMasterlist')}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <RecentActorsIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={'Manage Masterlist'} />
+                            </ListItemButton>
+                        </ListItem>
+                    </>
+                }
+
+                <ListItem onClick={Logout}>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <LogoutIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={'Logout'} />
+                    </ListItemButton>
+                </ListItem>
+
+            </List>
+        </Box>
+    );
+
     async function Logout() {
         dispatch(usermethod.Logout_User());
         navigate('/Login');
@@ -25,87 +153,38 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="bg-blue-500 shadow text-white bg-gradient-to-r from-sky-500 to-z-500">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between h-16 items-center">
-                    <div className="flex items-center">
-                        <p className="text-xl font-semibold flex items-center cursor-pointer" onClick={() => goToMainPage()}>
-                            {otherUserinfo.role !== '' ? <AdminPanelSettingsIcon className="mr-1" /> : ''}
-                            {
-                                otherUserinfo.role === "100" ?
-                                    'Adminpanel'
-                                    : otherUserinfo.role === "200" ? 'SuperAdminpanel' : ''
-                            }
-                        </p>
-                    </div>
-                    <div className="flex space-x-8">
-                        <ul className="flex space-x-6">
-                            <li>
-                                <button className="text-sm bg-blue-600 text-white py-2 px-2 rounded-lg hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 shadow-lg transform transition-transform hover:scale-105" onClick={() => navigate('/')}>Home</button>
-                            </li>
-                            {
-                                userinfo == null ?
-                                    <>
-                                        <li>
-                                            <button
-                                                className="text-sm p-2 bg-blue-600 text-white rounded-md hover:bg-blue-600 text-white-600 hover:text-white-500 transform transition-transform hover:scale-105"
-                                                onClick={() => navigate('/Login')}>
-                                                Login
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button
-                                                className="text-sm p-2 bg-blue-600 text-white rounded-md hover:bg-blue-600 text-white-500 hover:text-white-600 transform transition-transform hover:scale-105"
-                                                onClick={() => navigate('/Register')} >
-                                                Register
-                                            </button>
-                                        </li>
-                                    </>
-                                    :
-                                    <>
-                                        {otherUserinfo.role === '100' ?
-                                            <>
-                                                <li>
-                                                    <button className="text-sm p-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 text-white-500 hover:text-white-600 transform transition-transform hover:scale-105" onClick={() => navigate('/BusAdder')} to="/BusAdder">
-                                                        Add Bus
-                                                    </button>
-                                                </li>
-                                                <li>
-
-                                                    <button className="text-sm p-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 text-white-500 hover:text-white-600 transform transition-transform hover:scale-105" onClick={() => navigate('/ViewSeat')} to="/ViewSeat">
-                                                        View Seat
-                                                    </button>
-                                                </li>
-                                            </>
-                                            :
-                                            <>
-                                                <li>
-                                                    <button
-                                                        className="text-sm p-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 text-white-500 hover:text-white-600 transform transition-transform hover:scale-105"
-                                                        onClick={() => navigate('/Booking')}
-                                                    >
-                                                        Booking
-                                                    </button>
-                                                </li>
-                                            </>
-                                        }
-
-                                    </>
-                            }
-                        </ul>
-                        {
-                            userinfo &&
-                            <button
-                                className="bg-green-600 hover:bg-green-700 text-white px-3 py-2 rounded-md text-sm focus:outline transform transition-transform hover:scale-105"
-                                onClick={Logout}
+        <>
+            <nav className="bg-blue-500 shadow text-white bg-gradient-to-r from-blue-500 to-z-500">
+                <div className="max-w-7xl px-1 sm:px-1 lg:px-1">
+                    <div className="flex justify-start h-16 items-center">
+                        {userinfo ? <div>
+                            <IconButton
+                                color="inherit"
+                                className="mx-2"
+                                onClick={() => setDrawerOpen(true)}
                             >
-                                Logout  <i className="fa fa-sign-out ml-2" aria-hidden="true"></i>
-                            </button>
-                        }
+                                <MenuIcon />
+                            </IconButton>
+                        </div> : ''}
+                        <div className="flex items-center">
+                            <p className="text-xl font-semibold flex items-center cursor-pointer mx-5" onClick={() => goToMainPage()}>
+                                {
+                                    otherUserinfo.role === "100" ?
+                                        'Adminpanel'
+                                        : otherUserinfo.role === "200" ? 'SuperAdminpanel' : ''
+                                }
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav >
+            {userinfo ? <Drawer
+                open={drawerOpen}
+                onClose={() => setDrawerOpen(false)}
+            >
+                {list('left')}
+            </Drawer> : ''}
+        </>
     );
 };
 
